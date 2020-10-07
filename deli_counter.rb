34e001a8107e1c33d26a9af46ebcff1978b868b9
github username <github email address>
2020-10-07 13:05:47 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
+
 # Write your code here.
+def line(katz_deli)
+  size = katz_deli.size
+  position = 1
+  if size == 0
+    puts "The line is currently empty."
+  else 
+    position = 1
+    string_one = "The line is currently:"
+    katz_deli.each do |line|
+    string_one << " #{position}. #{line}"
+    position += 1
+    end
+    puts string_one
+  end
+end
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+   if katz_deli.size == 0 
+    puts "There is nobody waiting to be served!"
+   else 
+    serving = katz_deli.shift
+    puts "Currently serving #{serving}."
+   
+
+  end
+end
+now_serving(katz_deli)
